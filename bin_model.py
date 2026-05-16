45787d9a48a79d8aae0210758a4d79d7a6753af1
Ka Rol <93125845+karolleszkowicz@users.noreply.github.com>
2026-05-16 14:51:54 +0200

    Update bin_model.py
    Dodanie funkcji rysującej wykres zmian ceny opcji w czasie

--- bin_model.py
+++ bin_model.py
@@ -18,3 +18,40 @@ class BinomialModel:
                 tree[i, t] = self.d**i * self.u**(t - i) * self.S0
 
         return tree
+
+    def plot_tree(self, data_tree, title="Drzewo dwumianowe"):
+        """
+        :param title: Tytuł wykresu
+        :param data_tree: generated date tree from @generate_stock_tree function
+        :return: tree plot
+        """
+        plt.figure(figsize=(14, 8))
+
+        for t in range(self.N):
+            for i in range(t + 1):
+                x = t
+                y = data_tree[i, t]
+
+                # Wyznaczamy ceny w następnym kroku
+                # Ruch w górę wiersz pozostaje ten sam ruszamy się o jeden w góre
+                y_up = data_tree[i, t + 1]
+                # Ruch w dół oznacza zmiane wiesza i zmiane kolumny
+                y_down = data_tree[i + 1, t + 1]
+
+                # Rysowanie linii
+                plt.plot([x, x + 1], [y, y_up], 'g-', alpha=0.3, linewidth=1)
+                plt.plot([x, x + 1], [y, y_down], 'r-', alpha=0.3, linewidth=1)
+
+        # Rysowanie węzłów
+        for t in range(self.N + 1):
+            for i in range(t + 1):
+                x = t
+                y = data_tree[i, t]
+                plt.scatter(x, y, color='blue', s=30, zorder=5)
+
+
+        plt.title(title, fontsize=14)
+        plt.xlabel("Krok czasu (t)", fontsize=12)
+        plt.ylabel("Cena instrumentu", fontsize=12)
+        plt.grid(True, linestyle='--', alpha=0.6)
+        plt.show()

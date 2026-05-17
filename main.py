5af625e54607f9033fb48e6166a6bdc0d9eb99af
Aleksy Balazinski <54417806+AleksyBalazinski@users.noreply.github.com>
2026-05-17 14:42:59 +0200

    Analysis (#4)
    * add a small analysis
    
    * update requirements.txt

--- main.py
+++ main.py
@@ -3,14 +3,6 @@ from bin_model import BinomialModel
 from tree_plotter import plot_tree
 
 
-def get_down_factor(sigma, dt):
-    return np.exp(-1 * sigma * np.sqrt(dt))
-
-
-def get_up_factor(sigma, dt):
-    return np.exp(sigma * np.sqrt(dt))
-
-
 if __name__ == "__main__":
     bin_model = BinomialModel(S0=50, N=10, dt=1, u=1.2, d=0.8, r=0.05)
     plot_tree(bin_model.generate_stock_tree())

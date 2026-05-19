51f8f1a0903031e0013b019ccf081cf6c8c377fd
Ka Rol <93125845+karolleszkowicz@users.noreply.github.com>
2026-05-19 12:03:26 +0200

    Hedging (#7)
    * added hedging
    
    * funckja dodawania
    
    * dodanie funkcji price_with_hedging do klasy "EuropeanOption"
    
    * analiza hediging w jupyter
    
    * analiza hediging w jupyter
    
    * unitest dla delta hedgigu europeanCALL
    
    * niepotrzebne biblioteki i plik.py
    
    * aktualizacja requirment.txt

--- options.py
+++ options.py
@@ -30,6 +30,41 @@ class EuropeanOption(Option):
 
         return opt_tree[0, 0]
 
+    # wyznaczenie dealta hedging -> delta aktywo i alfa gotówki
+    def price_with_hedging(self, model: BinomialModel):
+        stock_tree = model.generate_stock_tree()
+        N = model.N
+
+        opt_tree = RecombinantTree(N)
+        delta_tree = RecombinantTree(N - 1)
+        alpha_tree = RecombinantTree(N - 1)
+
+        # Warunek końcowy
+        opt_tree.set_step(N, self.payoff(stock_tree.get_step(N)))
+
+        # wyliczamy wstecz ceny hedingu dla każdego węzłą
+        for t in range(N - 1, -1, -1):
+            for i in range(t + 1):
+                # ustalamy cene obligacji i wartość aktywa w danej chwili czasu
+                V_up = opt_tree[i, t + 1]
+                V_down = opt_tree[i + 1, t + 1]
+                S_up = stock_tree[i, t + 1]
+                S_down = stock_tree[i + 1, t + 1]
+
+                # Wyliczanie delty i alfy -> rozwiązanie ukłądu równań z wykładu
+                delta = (V_up - V_down) / (S_up - S_down)
+                df = np.exp(-model.r * model.dt)
+                alpha = df * (V_up - delta * S_up)
+
+                delta_tree[i, t] = delta
+                alpha_tree[i, t] = alpha
+
+                # wyliczenie kosztu budowy porfela -> czy zgadza się z protfolio_vla w ujpyterze
+                opt_tree[i, t] = delta * stock_tree[i, t] + alpha
+
+        return opt_tree, delta_tree, alpha_tree
+
+
 
 class AmericanOption(Option):
     def payoff(self, S):
@@ -51,6 +86,7 @@ class AmericanOption(Option):
                 opt_tree[i, t] = max(cont, exercise)
 
         return opt_tree[0, 0]
+
 
 
 class AmericanCall(AmericanOption):
